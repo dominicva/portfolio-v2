ee7e6db21f51e2d676961d3f65e9e36f63fb02b1
Dominic van Almsick <dominicvana@gmail.com>
2023-06-23 13:06:12 +0100

    chore: first commit

---
+++ lib/font.ts
@@ -0,0 +1,6 @@
+import { Overpass_Mono } from 'next/font/google';
+
+export const overPassMono = Overpass_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,18 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: '#121421',
+        'white-dim': '#ffffffbf',
+        'primary-light': '#383a48',
+        'primary-dark': '#000',
+        secondary: '#36f1cd',
+        'secondary-light': '#7bffff',
+        'secondary-dark': '#00bd9c',
+        error: '#ff6663',
+      },
     },
+
+    plugins: [],
   },
-  plugins: [],
-}
+};
