1f6dadfd7f1aaddff2fca93ed4706882a0a34b00
Dominic van Almsick <dominicvana@gmail.com>
2023-06-23 13:22:05 +0100

    feat: add initial header

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={overPassMono.className}>{children}</body>
+      <body className={`${overPassMono.className} bg-primary text-white`}>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-primary">
-      <h1 className="text-white">Dom van Almsick</h1>
-    </main>
+    <div className="p-6">
+      <header className="">
+        <nav className="">
+          <ul className="flex gap-6 text-secondary">
+            <li>
+              <a
+                href="https://standardresume.co/r/dominicva"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:underline hover:underline-offset-4"
+              >
+                Resume
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/dominicva"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:underline hover:underline-offset-4"
+              >
+                GitHub
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-primary">
+        <h1 className="text-white">Dom van Almsick</h1>
+      </main>
+    </div>
   );
 }
