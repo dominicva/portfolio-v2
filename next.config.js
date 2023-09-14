b3beb64f58cabe97a7fc4dbddaaedab5c03d1b60
Dominic van Almsick <dominicvana@gmail.com>
2023-09-14 22:39:11 +0100

    fix: rehype

--- next.config.js
+++ next.config.js
@@ -1,27 +1,5 @@
-const rehypePrettyCode = require('rehype-pretty-code');
-
-/** @type {import('rehype-pretty-code').Options} */
-const options = {
-  theme: 'one-dark-pro',
-  onVisitLine(node) {
-    if (node.children.length === 0) {
-      node.children = [{ type: 'text', value: '' }];
-    }
-  },
-  onVisitHighlightedLine(node) {
-    node.properties.className.push('highlighted');
-  },
-  onVisitHighlightedWord(node) {
-    node.properties.className = ['highlighted', 'word'];
-  },
-};
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [[rehypePrettyCode, options]],
-  },
   experimental: {
     // mdxRs: true,
   },
