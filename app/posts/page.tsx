f92a6b4a850a2437c3fcdb3a9f9837332eaa607f
Dominic van Almsick <dominicvana@gmail.com>
2023-09-14 15:41:00 +0100

    feat: render blog post previews

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,13 +1,25 @@
 import Link from 'next/link';
+import { getSortedPostsMetaData } from '@/lib/posts';
 
-export default function BlogPage() {
+export default async function BlogPage() {
+  const posts = await getSortedPostsMetaData();
+
   return (
-    <div>
-      <h1>Blog</h1>
-      <ul>
-        <li>
-          <Link href="/posts/intro-typescript-react">First Post</Link>
-        </li>
+    <div className="my-12">
+      <h1 className="mb-10 text-3xl">Blog</h1>
+      <ul className="flex flex-col gap-8">
+        {posts.map(({ slug, title, author, date }) => (
+          <li key={slug}>
+            <Link href={`/posts/${slug}`}>
+              <hgroup>
+                <h2>{title}</h2>
+                <p>by {author}</p>
+              </hgroup>
+
+              <small>{date}</small>
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
--- lib/posts.ts
+++ lib/posts.ts
@@ -4,6 +4,13 @@ import { compileMDX } from 'next-mdx-remote/rsc';
 
 const ROOT_DIRECTORY = path.join(process.cwd(), 'content');
 
+interface PostMetaData {
+  slug: string;
+  author: string;
+  title: string;
+  date: string;
+}
+
 export const getPostBySlug = async (slug: string) => {
   const filePath = path.join(ROOT_DIRECTORY, `${slug}.mdx`);
   const fileContent = fs.readFileSync(filePath, 'utf-8');
@@ -19,4 +26,29 @@ export const getPostBySlug = async (slug: string) => {
     content,
     frontmatter,
   };
+};
+
+export const getSortedPostsMetaData = async () => {
+  const fileNames = fs.readdirSync(ROOT_DIRECTORY);
+
+  const allPostsMetaData: PostMetaData[] = await Promise.all(
+    fileNames.map(async fileName => {
+      const slug = fileName.replace('.mdx', '');
+
+      const { frontmatter } = await getPostBySlug(slug);
+
+      return {
+        slug,
+        ...(frontmatter as Omit<PostMetaData, 'slug'>),
+      };
+    })
+  );
+
+  return allPostsMetaData.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1;
+    }
+
+    return -1;
+  });
 };
