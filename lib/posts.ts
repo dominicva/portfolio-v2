3fe0527553ec3fe081b83534b762d444b381d82b
Dominic van Almsick <dominicvana@gmail.com>
2023-09-15 09:01:27 +0100

    fix: shiki not using import

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,15 +1,72 @@
 import path from 'path';
 import fs from 'fs';
 import { compileMDX } from 'next-mdx-remote/rsc';
-import rehypePrettyCode from 'rehype-pretty-code';
+// import rehypePrettyCode from 'rehype-pretty-code';
+import rehypePrettyCode, {
+  Options as RehypeCodeOptions,
+} from 'rehype-pretty-code';
+
+import * as shiki from 'shiki';
+import { PluggableList } from 'unified';
 import { components } from '@/components/MDXComponents';
 import type { PostMetaData } from '@/types';
 
 const ROOT_DIRECTORY = path.join(process.cwd(), 'content');
 
-const prettyCodeOptions = {
-  theme: 'one-dark-pro',
+// Shiki loads languages and themes using "fs" instead of "import", so Next.js
+// doesn't bundle them into production build. To work around, we manually copy
+// them over to our source code (lib/shiki/*) and update the "paths".
+//
+// Note that they are only referenced on server side
+// See: https://github.com/shikijs/shiki/issues/138
+const getShikiPath = (): string => {
+  return path.join(process.cwd(), 'lib/shiki');
 };
+
+const touched = { current: false };
+
+// "Touch" the shiki assets so that Vercel will include them in the production
+// bundle. This is required because shiki itself dynamically access these files,
+// so Vercel doesn't know about them by default
+const touchShikiPath = (): void => {
+  if (touched.current) return; // only need to do once
+  fs.readdir(getShikiPath(), err => console.error(err)); // fire and forget
+  touched.current = true;
+};
+
+const getHighlighter: RehypeCodeOptions['getHighlighter'] = async options => {
+  touchShikiPath();
+
+  const highlighter = await shiki.getHighlighter({
+    // This is technically not compatible with shiki's interface but
+    // necessary for rehype-pretty-code to work
+    // - https://rehype-pretty-code.netlify.app/ (see Custom Highlighter)
+    ...(options as any),
+    paths: {
+      languages: `${getShikiPath()}/languages/`,
+      themes: `${getShikiPath()}/themes/`,
+    },
+  });
+
+  return highlighter;
+};
+
+const getRehypeCodeOptions = (): Partial<RehypeCodeOptions> => ({
+  // Requirements for theme:
+  // - Has light and dark version
+  // - Uses italic in several places
+  theme: { light: 'github-light', dark: 'github-dark-dimmed' },
+  // Need to use a custom highlighter because rehype-pretty-code doesn't
+  // let us customize "paths".
+  getHighlighter,
+});
+
+export const getMdxCode = (): PluggableList => [
+  [rehypePrettyCode, getRehypeCodeOptions()],
+];
+// const prettyCodeOptions = {
+//   theme: 'one-dark-pro',
+// };
 
 export const getPostBySlug = async (slug: string) => {
   const filePath = path.join(ROOT_DIRECTORY, `${slug}.mdx`);
@@ -21,7 +78,8 @@ export const getPostBySlug = async (slug: string) => {
     options: {
       parseFrontmatter: true,
       mdxOptions: {
-        rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+        // rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+        rehypePlugins: getMdxCode(),
       },
     },
   });
--- next.config.js
+++ next.config.js
@@ -1,9 +1,24 @@
+const rehypePrettyCode = require('rehype-pretty-code');
+const fs = require('fs');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    // mdxRs: true,
-  },
+  // experimental: {
+  // mdxRs: true,
+  // },
 };
 
-const withMDX = require('@next/mdx')();
+/** @type {import('rehype-pretty-code').Options} */
+const options = {
+  theme: 'dracula',
+};
+
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [[rehypePrettyCode, options]],
+  },
+});
+
 module.exports = withMDX(nextConfig);
